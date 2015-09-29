91f097b87ee0983225b08c29b9e793d0c0883d26
Emile Badenhorst <MyvarHD@gmail.com>
2015-09-29 19:58:56 +0200

    added basic syntax hilighting

---
+++ RustIDE/RustIDE/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RustIDE
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(-1, 100);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(1118, 37);
+            this.materialTabSelector1.TabIndex = 0;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(-1, 137);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(1118, 486);
+            this.materialTabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1110, 460);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(-1, 64);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(39, 36);
+            this.materialFlatButton1.TabIndex = 2;
+            this.materialFlatButton1.Text = "File";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 622);
+            this.Controls.Add(this.materialTabControl1);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Name = "MainForm";
+            this.Text = "RastIDE";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ RustIDE/RustIDE/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using FastColoredTextBoxNS;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RustIDE
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            materialTabControl1.TabPages.Clear();
+            AddEditorTab("new");
+        }
+
+
+        private void AddEditorTab(string name)
+        {
+            TabPage p = new TabPage(name);
+            var textbox = new FastColoredTextBox() { Dock = DockStyle.Fill };
+            p.Controls.Add(textbox);
+            textbox.TextChanged += Textbox_TextChanged;
+            materialTabControl1.TabPages.Add(p);
+        }
+
+        Style GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Regular);
+        Style PurpleStyle = new TextStyle(Brushes.Purple, null, FontStyle.Regular);
+        Style BlueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Regular);
+        Style BlackStyle = new TextStyle(Brushes.Black, null, FontStyle.Regular);
+
+        private void Textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var fastColoredTextBox1 = sender as FastColoredTextBox;
+
+            e.ChangedRange.ClearStyle(GreenStyle);
+            e.ChangedRange.ClearStyle(PurpleStyle);
+            e.ChangedRange.ClearStyle(BlueStyle);
+            e.ChangedRange.ClearStyle(BlackStyle);
+
+            fastColoredTextBox1.AddStyle(GreenStyle);
+            fastColoredTextBox1.AddStyle(BlueStyle);
+            fastColoredTextBox1.AddStyle(BlackStyle);
+            fastColoredTextBox1.AddStyle(PurpleStyle);
+
+            e.ChangedRange.SetStyle(GreenStyle, @"//.*$", RegexOptions.Multiline);
+            e.ChangedRange.SetStyle(GreenStyle, "\\/\\*.+\\*\\/", RegexOptions.Multiline);
+
+            e.ChangedRange.SetStyle(PurpleStyle, @"let\s|fn\s|\sin\s|for\s|while\s|match\s|\smut\s");
+
+            e.ChangedRange.SetStyle(BlueStyle, @"fn (?<range>([A-za-z]([A-Za-z]|[0-9])+))");
+            e.ChangedRange.SetStyle(BlueStyle, "\".+\"");
+            e.ChangedRange.SetStyle(BlueStyle, "[0-9]");
+
+
+            e.ChangedRange.SetStyle(BlackStyle, @"([A-za-z]([A-Za-z]|[0-9])+)!");
+
+
+
+            e.ChangedRange.ClearFoldingMarkers();
+            e.ChangedRange.SetFoldingMarkers("{", "}");
+            e.ChangedRange.SetFoldingMarkers(@"#region\b", @"#endregion\b");
+        }
+    }
+}
+
